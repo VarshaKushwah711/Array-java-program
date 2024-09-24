84e5eebbc7bb43e42378726af43b01f18639bd2e
Varsha Kushwah <varshakushwah711@gmail.com>
2024-09-25 00:00:11 +0530

    Add files via upload

---
+++ Array23.java
@@ -0,0 +1,13 @@
+//1.Create an array with the numbers 1 to 5.
+public class Array23 {
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50};
+        for(int i : arr)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
+
+

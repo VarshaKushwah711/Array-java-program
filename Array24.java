1c2d11a4cd8322d94cfb80a461debc5156501fb9
Varsha Kushwah <varshakushwah711@gmail.com>
2024-09-28 23:02:45 +0530

    Add files via upload

---
+++ Array24.java
@@ -0,0 +1,8 @@
+//2. Access the first element of the array [10, 20, 30, 40, 50].
+public class Array24 {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40, 50};
+        System.out.println("first element of the array: " + arr[0]);
+    }
+    
+}

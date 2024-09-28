c978177b8f4a527094f1b1f174274ed16d123ca1
Varsha Kushwah <varshakushwah711@gmail.com>
2024-09-28 23:04:30 +0530

    Add files via upload

---
+++ Array25.java
@@ -0,0 +1,10 @@
+//3. Access the last element of the array [apple, mango, banana, grapes, orange].
+public class Array25 {
+    public static void main(String[] args) {
+
+        String arr[] = {"apple", "mango", "banana", "grapes", "orange"};
+        System.out.println("Last element of the array: " + arr[arr.length-1] );
+        
+    }
+    
+}

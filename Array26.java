6fbff450c8c4e438b61df66b944c28c1747c76a4
Varsha Kushwah <varshakushwah711@gmail.com>
2024-09-29 20:13:01 +0530

    Add files via upload

---
+++ Array26.java
@@ -0,0 +1,9 @@
+//4. Find the length of the array [7, 14, 21, 28].
+public class Array26 {
+    
+    public static void main(String[] args) {
+        
+        int arr[] = {7, 14, 21, 28 , 35};
+        System.out.println("Length of the array: " + arr.length);
+    }
+}
